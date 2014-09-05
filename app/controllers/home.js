4fcdf357c1f0d0f0c3d24acc1b8f8ff17148cc23
Igor Tomov <igor.tomov@globallogic.com>
2014-09-05 11:47:08 +0300

    Temporary switched off usage of MongoDB, instead game data retrieve from dump file 'quizzes.json'

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     config  = require('../../config/config'),
-    router = express.Router(),
-    mongoose = require('mongoose'),
-    Quiz = mongoose.model('Quiz');
+    router = express.Router();
 
 module.exports = function (app) {
   app.use('/', router);
--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -1,19 +1,29 @@
 var express     = require('express'),
     router      = express.Router(),
+    fs          = require("fs");/*,
     mongoose    = require('mongoose'),
-    Quiz        = mongoose.model('Quiz');
+    Quiz        = mongoose.model('Quiz');*/
 
 module.exports = function (app) {
     app.use('/', router);
 };
 
 router.get( "/quiz/demo", function( req, res, next ){
-    Quiz.find()
+    /*Quiz.find()
         .select( {_id: false, __v: false} )
         .exec( function( err, quiz ){
             if (err) return console.error(err);
 
             res.setHeader( "Content-Type", "application/json" );
             res.end( JSON.stringify( quiz ) );
-        });
+        });*/
+
+    fs.readFile( "quizzes.json", {encoding: "UTF-8"}, function( err, data ){
+        if ( err ){
+            throw err;
+        }
+
+        res.setHeader( "Content-Type", "application/json" );
+        res.end( data );
+    });
 });
